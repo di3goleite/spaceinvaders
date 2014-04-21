710266cf4ad2a38ff1f4e4e7bb961c1aae75dd10
v-assys <lukas.ecomp@gmail.com>
2014-04-21 16:16:01 -0300

    Modelo base da interface pronta

---
+++ SpaceInvaders/src/view/Fase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package view;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Vinícius Assis
+ */
+public class Fase extends JPanel implements IFase{
+
+    @Override
+    public void addPersonagem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
