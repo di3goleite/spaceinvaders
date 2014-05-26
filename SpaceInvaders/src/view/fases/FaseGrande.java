2177cbc8ee65e03d601b6b4a276aa9973833a6d9
lucasvinicius <lucasvinicius@netmedia4-Pegatron>
2014-05-26 19:32:54 -0300

    Inicio da implementaçao do game Loop (Lucas Vinicius)

--- SpaceInvaders/src/view/fases/FaseGrande.java
+++ SpaceInvaders/src/view/fases/FaseGrande.java
@@ -18,7 +18,7 @@ import javax.swing.JPanel;
  *
  * @author Vinícius Assis
  */
-public class FaseGrande extends JPanel {
+public class FaseGrande extends JPanel implements IGameLoop {
 
     private BufferedImage backBuffer;
     private ImageIcon fundo;
@@ -30,120 +30,39 @@ public class FaseGrande extends JPanel {
     public FaseGrande() {
         this.setBackImage("src/interface1/multimidia/imagens/cenas/planoFundo.jpg");
         backBuffer = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
-       
     }
-
-  
-    public void criaInimigos(int posicaoX, int posicaoY) {
-     //   this.inimigos = new Inimigos[5][16];
-        int x, y = posicaoY;
-     for(int i = 0; i < 5; i++){
-            x = posicaoX;
-            y += 25;
-            for(int j = 0; j < 16; j++){
-                if(i == 0 || i == 1){
-             //       inimigos[i][j] = new Inimigos("src/interface1/multimidia/"
-              //              + "imagens/personagens/inimigo1-1.jpg", x, y);
-                }
-                else if (i == 2 || i == 3){
-           //         inimigos[i][j] = new Inimigos("src/interface1/multimidia/"
-                      //      + "imagens/personagens/inimigo2-1.jpg", x, y);
-                }
-                else{
-             //       inimigos[i][j] = new Inimigos("src/interface1/multimidia/"
-              //              + "imagens/personagens/inimigo3-1.jpg", x, y);
-                }
-                x+=40;
-            }
-        }   
-    }
-    
     
     public void setBackImage(String URL) {
         this.fundo = new ImageIcon(URL);
     }
-    public void atualizar() {
-       
+    //Metodos que sao necessarios para a implementacao do game loop
+    @Override
+     public void processInput(){
+        
     }
+    @Override
+     public void update(){
+        
+    }
+    @Override
+    public void render(){
+       this.repaint();
+    }
+    
 
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         super.paintComponent(g2);
         Graphics bbg = backBuffer.getGraphics();
-
-         //Parte onde o fundo eh desenhado
+        //Parte onde o fundo eh desenhado
         //Fundo com dimensoes de 800x600
         bbg.drawImage(fundo.getImage(), 0, 0, this);
-
         //Parte onde os elementos da tela sao modificado atraves de outra variavel
         //Caso isso nao seja feito toda a tela sera modificada a cada vez
         Graphics2D bbg2d = (Graphics2D) backBuffer.getGraphics();
         bbg2d.drawImage(naveAmiga.getImage(),this.posicaoNAmigaX,this.posicaoNAmigaY,this);
-        
-        this.criaInimigos(this.posicaoNInimigaX, this.posicaoNInimigaY);
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 16; j++){
-           //     bbg2d.drawImage(inimigos[i][j].getImage(),
-           //             this.inimigos[i][j].getPosicaoX(),
-            //            this.inimigos[i][j].getPosicaoY(),this);   
-            }
-        }
-        
-        //================================================================================== 
         g2.drawImage(backBuffer, 0, 0, this);//Desenha tudo o que foi alterado no painel
     }
-
    
-	/**
-	 * Classe que e usada como Thread para fazer modificacoes mas posicoes dos
-	 * inimigos na tela
-	 * 
-	 * @author Lucas Vinicius
-	 *
-	 * @see PainelCentral
-	 */
-	class MoveInimigos implements Runnable {
-            private int fpsInimigos = 1;
-            private boolean indo = true;
-		public MoveInimigos() {
-		}
-
-		@Override
-		public void run() {
-                    posicaoNInimigaX = 15;
-                    while(true){
-                        if(posicaoNInimigaX > 10 && posicaoNInimigaX < 150){
-                            if(indo){
-                                posicaoNInimigaX+=10;
-                            }
-                            else if(!indo){
-                                posicaoNInimigaX-=10;
-                            }
-                        }
-                        else{
-                            posicaoNInimigaY+=20;
-                            if(indo){
-                                indo = false;
-                                posicaoNInimigaX = 145;
-                            }
-                            else{
-                                indo = true;
-                                posicaoNInimigaX = 15;
-                            }
-                        }
-                        try {
-                            Thread.sleep(1000/2);
-                        } catch (InterruptedException ex) {
-                            System.out.println("Thread MoveInimigo falhou");
-                        }
-                        
-                       repaint();
-                    }
-		
-		}
-
-	}
-   
-
 }
